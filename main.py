6bb1d3925bd9e37ccecc4240f26c57974d287891
daiyadeguchi <double.d.rugby@gmail.com>
2023-11-30 20:33:16 +0900

    dynamically change urls

--- main.py
+++ main.py
@@ -3,13 +3,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-if __name__ == '__main__':
-    response = requests.get("https://www.songkick.com/artists/127504-lamb-of-god/calendar")
-    soup = BeautifulSoup(response.content, "html.parser")
-    locs = soup.select('.primary-detail')
-    result = {}
-    for loc in locs:
-        for japan in ["JPN", "Japan", "Tokyo"]:
-            if(japan in loc):
-                result = {"LoG": True}
+urls = {
+    "lamb of god": "https://www.songkick.com/artists/127504-lamb-of-god/calendar",
+    "julian lage": "https://www.songkick.com/artists/988278-julian-lage/calendar",
+    "metallica": "https://www.songkick.com/artists/331163-metallica/calendar",
+    "ffdp": "https://www.songkick.com/artists/577269-five-finger-death-punch/calendar",
+    "slipknot": "https://www.songkick.com/artists/1012485-slipknot/calendar",
+    "jason mraz": "https://www.songkick.com/artists/336656-jason-mraz/calendar",
+    }
 
+if __name__ == '__main__':
+    result = {}
+    for key in urls:
+        response = requests.get(urls[key])
+        soup = BeautifulSoup(response.content, "html.parser")
+        locs = soup.select('.primary-detail')
+        for loc in locs:
+            for japan in ["JPN", "Japan", "Tokyo"]:
+                if(japan in str(loc)):
+                    result = {key: True}
+
+        print(result)
