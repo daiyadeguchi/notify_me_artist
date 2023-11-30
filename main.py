20a7fb2714346fc1a29a8b4bb13d07e3bb09246a
daiyadeguchi <double.d.rugby@gmail.com>
2023-11-30 20:10:14 +0900

    Implement japan finder for lamb of god

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import requests
 from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
-    response = requests.get("https://www.lamb-of-god.com/tour/")
+    response = requests.get("https://www.songkick.com/artists/127504-lamb-of-god/calendar")
     soup = BeautifulSoup(response.content, "html.parser")
-    locs = soup.select('div')
+    locs = soup.select('.primary-detail')
+    result = {}
     for loc in locs:
-        print(loc)
+        for japan in ["JPN", "Japan", "Tokyo"]:
+            if(japan in loc):
+                result = {"LoG": True}
+
